f7b66dd8405b44ce5e71540b93bc580187d3d18e
Nikola Kovacs <nikola.kovacs@gmail.com>
2016-06-22 00:53:40 +0200

    Don't crash if crontab doesn't exist or is invalid
    Wait for it to be created or fixed

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/namsral/flag"
 	"gopkg.in/fsnotify.v1"
 	"log"
@@ -26,26 +27,26 @@ func init() {
 	flag.IntVar(&numCPU, "cpu", runtime.NumCPU(), "maximum number of CPUs")
 }
 
-func parseFile(filePath string) *Runner {
+func parseFile(filePath string) (*Runner, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		stderr.Fatalf("crontab path: %v error: %v", filePath, err)
+		return nil, fmt.Errorf("crontab path: %v error: %v", filePath, err)
 	}
 	defer file.Close()
 
 	parser, err := NewParser(file)
 	if err != nil {
-		stderr.Fatalf("Parser read error: %v", err)
+		return nil, fmt.Errorf("Parser read error: %v", err)
 	}
 	parser.SetLogger(stdout)
 	parser.SetErrorLogger(stderr)
 
 	runner, err := parser.Parse()
 	if err != nil {
-		stderr.Fatalf("Parser parse error: %v", err)
+		return nil, fmt.Errorf("Parser parse error: %v", err)
 	}
 
-	return runner
+	return runner, nil
 }
 
 func main() {
@@ -77,14 +78,23 @@ func main() {
 				if event.Op&(fsnotify.Remove|fsnotify.Rename) != 0 {
 					// removed or renamed, stop runners
 					stdout.Printf("Crontab removed, stopping")
-					runner.Stop()
+					if runner != nil {
+						runner.Stop()
+					}
 					break
 				}
 
 				if event.Op&(fsnotify.Create|fsnotify.Write) != 0 {
 					stdout.Printf("Reloading crontab")
-					runner.Stop()
-					runner = parseFile(crontabPath)
+					if runner != nil {
+						runner.Stop()
+					}
+					var err error
+					runner, err = parseFile(crontabPath)
+					if err != nil {
+						stderr.Print(err.Error())
+						break
+					}
 					runner.Start()
 				}
 			case err, ok := <-watcher.Errors:
@@ -97,13 +107,18 @@ func main() {
 		}
 	}()
 
-	runner = parseFile(crontabPath)
+	runner, err = parseFile(crontabPath)
+	if err != nil {
+		stderr.Print(err.Error())
+	}
 	dir := filepath.Dir(crontabPath)
+
+	if runner != nil {
+		runner.Start()
+	}
 
 	var wg sync.WaitGroup
 	shutdown(runner, &wg, stdout)
-
-	runner.Start()
 	wg.Add(1)
 
 	watcher.Add(dir)
@@ -119,7 +134,9 @@ func shutdown(runner *Runner, wg *sync.WaitGroup, logger *log.Logger) {
 		s := <-c
 		logger.Println("Got signal: ", s)
 		watcher.Close()
-		runner.Stop()
+		if runner != nil {
+			runner.Stop()
+		}
 		wg.Done()
 	}()
 }
